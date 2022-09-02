2b5fb220485096be7362c559f0323bdd2cd3d730
Franzua Plasencia <69611157+plasenca@users.noreply.github.com>
2022-09-02 20:33:30 +0000

    Python - Exercise 1 Refactor

--- Python Exercises/Probabilities for Sums in Rolling Cubic Dice/main.py
+++ Python Exercises/Probabilities for Sums in Rolling Cubic Dice/main.py
@@ -1,1 +1,27 @@
-print("Hola Mundo")
+import log_file as log
+
+from collections import Counter
+from itertools import combinations_with_replacement
+
+def rolldice_sum_prob(num, quant_dies):
+    # Initialize local variables
+    cont = 0
+    list_matched = []
+    LIST_VALUES = [1, 2, 3, 4, 5, 6]*quant_dies
+    
+    # All numbers for each die
+    
+    total_combinations = list(set(combinations_with_replacement(LIST_VALUES, quant_dies)))
+
+    list_ = map()
+    
+    for combination in total_combinations:
+        sum_total = sum(combination)
+        if sum_total == num:
+            list_matched.append(combination)
+            cont+=1
+    
+    print(f"Conteo: {cont}\nDivisión: {cont}/{pow(6,quant_dies)}")
+    return cont/pow(6,quant_dies)
+
+print(rolldice_sum_prob(35, 7))
--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Hi there!! This is recopilation from [Codewars](www.codewars.com). [Codewars](www.codewars.com) is a page where you can practise the programming languages that you want doing exercises that they propose to you.
 
-Notice, this proyect is totally done on Replit
+Notice, this proyect is totally done on Replit.
+
+## [Python Exercises]()
+
