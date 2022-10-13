2895017c01dc70d9cd6439f4d7dc11a16d8d3584
Franzua Plasencia <franzua.15pc@gmail.com>
2022-10-13 06:38:46 -0500

    Finish Roman Numbers

--- Python Exercises/Roman Numerals Decoder/roman_number.py
+++ Python Exercises/Roman Numerals Decoder/roman_number.py
@@ -16,18 +16,22 @@ def solution(roman_num: str) -> int|str:
         
         # Optional - Validate the form of the Roman Numeral
         # print(Counter(roman_num))
-        
+
         # Functionality
-        list_num_convert = list(map(lambda l: [value for letter, value in dicc_roman_num.items() if letter==l], roman_num))
+        list_num_convert = list(map(lambda l: [value for letter, value in dicc_roman_num.items() if letter==l][0], roman_num))
         list_num_convert = list_num_convert[::-1]
-        print(list_num_convert)
         
-        for i in list_num_convert:
-            
-            if list_num_convert[i][0] < list_num_convert[i+1][0]:
-                num_total += list_num_convert[i][0]
-            
+        for i, num in enumerate(list_num_convert):
+            if i == 0: # First number
+                num_total += num
+            elif num < list_num_convert[i-1]: # If the number is less than the previous number
+                num_total -= num
+            else: # If the number is greater than the previous number
+                num_total += num
+        
+        return num_total
+        
     else: return f"The Roman Numeral({roman_num}) is not a string"
     
 if __name__ == '__main__':
-    solution("MDCLXVI")
+    print(solution("MCMXC"))
